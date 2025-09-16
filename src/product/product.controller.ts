07c05302de6395635a9eeb40fe91241c3efa9ecb
sajjad <sajjadsajjad098765@gmail.com>
2025-09-16 16:22:12 +0600

    14 module done

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,23 +1,25 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
 
 @Controller('product')
 export class ProductController {
-    // constructor is a special function that connects the controller to the service
-    constructor(private readonly productService: ProductService) {}
+  // constructor is a special function that connects the controller to the service
+  constructor(private readonly productService: ProductService) {}
 
-    //get all products
-    @Get()
-    getAllProducts() {
-        return this.productService.getAllProducts();
-    }
+  //get all products
+  @Get()
+  @UseGuards(AuthGuard)
+  getAllProducts() {
+    return this.productService.getAllProducts();
+  }
 
-    //get product by id
-    @Get(':id')
-    getProductById(@Param('id') id: string) {
-        return this.productService.getProductById(Number(id));
-    }
+  //get product by id
+  @Get(':id')
+  getProductById(@Param('id') id: string) {
+    return this.productService.getProductById(Number(id));
+  }
 }
 
 //! start in : 6 no video
